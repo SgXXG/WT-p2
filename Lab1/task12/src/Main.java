b9400b97ce15645c92b708113be25190a053b2e7
Stanislav <72697394+SgXXG@users.noreply.github.com>
2022-09-14 13:53:49 +0300

    lab1 task12

---
+++ Lab1/task12/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Book Book1 = new Book("book1");
+        Book1.setAuthor("author1");
+        Book1.setPrice(100);
+        Book1.setEdition(2);
+
+        System.out.println(Book1);
+
+        Book Book2 = new Book("book1");
+        Book2.setAuthor("author1");
+        Book2.setPrice(200);
+        Book2.setEdition(2);
+
+        System.out.println(Book2);
+
+        if (Book1.equals(Book2)){
+            System.out.println("Equals");
+        }
+        else{
+            System.out.println("NOT Equals");
+        }
+
+    }
+}
