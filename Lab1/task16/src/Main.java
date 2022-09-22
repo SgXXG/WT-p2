f3f1d71410f581adc27b27f6008ec65e8c25fd0e
Stanislav <72697394+SgXXG@users.noreply.github.com>
2022-09-22 13:05:30 +0300

    lab1 task16

--- Lab1/task16/src/Main.java
+++ Lab1/task16/src/Main.java
@@ -1,11 +1,14 @@
+import java.util.Comparator;
+import java.util.List;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         TreeSet<Book> bookArrayList = new TreeSet<Book>();
 
         Book Book1 = new Book("book1");
-        Book1.setAuthor("author1");
+        Book1.setAuthor("bauthor1");
         Book1.setPrice(100);
         Book1.setEdition(2);
         Book1.setISBN(123);
@@ -13,7 +16,7 @@ public class Main {
         System.out.println(Book1);
 
         Book Book2 = new Book("aook1");
-        Book2.setAuthor("author1");
+        Book2.setAuthor("cauthor1");
         Book2.setPrice(200);
         Book2.setEdition(2);
         Book2.setISBN(135);
@@ -39,19 +42,46 @@ public class Main {
             System.out.println("NOT Equals");
         }
 
+        // Print before sort
         System.out.println("");
         System.out.println("Before sort");
         for (Book h: bookArrayList) {
             System.out.println(h);
         }
-
         System.out.println("");
         System.out.println("After sort");
+
+        // Sort by title
+        List<Book> sSort = bookArrayList.stream().sorted(Comparator.comparing(b -> b.getTitle(b))).collect(Collectors.toList());
+        System.out.println("");
+        System.out.println("Sort by Title");
+        for (Book h: sSort) {
+            System.out.println(h);
+        }
+
+        // Sort by title & author
+        sSort = bookArrayList.stream().sorted(Comparator.comparing(b -> b.getAuthor())).collect(Collectors.toList());
+        System.out.println("");
+        System.out.println("Sort by Title & Author");
+        for (Book h: sSort) {
+            System.out.println(h);
+        }
+
+        // Sort by author & title
+        sSort = bookArrayList.stream().sorted(Comparator.comparing(b -> b.getTitle(b))).collect(Collectors.toList());
+        System.out.println("");
+        System.out.println("Sort by Author & Title");
+        for (Book h: sSort) {
+            System.out.println(h);
+        }
+
+        // Sort by author & title & price
+        System.out.println("");
+        System.out.println("Sort by Author & Title & Price");
         bookArrayList.stream().sorted(Book.comparatorBookPrice);
 
         for (Book h: bookArrayList) {
             System.out.println(h);
         }
-
     }
 }
